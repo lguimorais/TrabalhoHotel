7ec85b3fc64071f6f9f20970c75b7f21976459cc
Thiago Emanuel <thiagoeajds@gmail.com>
2024-11-06 16:13:22 -0300

    Att

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
         System.out.println("└————————————————————————┴————————————————————————┘");
         escolha = scanf.nextInt();
         if (escolha == 1){
-            quartoStandard.salvarCadastroEmArquivo("CadastroQuartos.txt");
+            quartoStandard.salvarCadastroEmArquivo("CadastroQuartos.txt");ljkhjkh
         } else {
             System.out.println("saindo...");
         }
