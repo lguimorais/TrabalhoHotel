cef64cf2581e15a9decb504e981d2a5d0f2d0298
Thiago Emanuel <thiagoeajds@gmail.com>
2024-11-06 15:13:50 -0300

    Att

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("┌—————————————————————————————————————————————————┐");
+        System.out.println("│                                                 │");
+        System.out.println("│                                                 │");
+        System.out.println("│                                                 │");
+        System.out.println("├—————————————————————————————————————————————————┤");
+        System.out.println("│                                                 │");
+        System.out.println("│                                                 │");
+        System.out.println("│                                                 │");
+        System.out.println("└—————————————————————————————————————————————————┘");
 
 }
 }
